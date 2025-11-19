2548196836913e93402e5aebb5cff767e5f7246e
bando9 <bandomega1@gmail.com>
2025-11-20 03:27:36 +0700

    feat: time picker input

--- src/modules/task/task-list.tsx
+++ src/modules/task/task-list.tsx
@@ -19,6 +19,9 @@ export function TaskList() {
 
     const formData = new FormData(event.currentTarget);
 
+    const time = formData.get("time-picker");
+    console.log(time);
+
     const newId = tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1;
 
     const newTask: Task = {
@@ -87,6 +90,19 @@ export function TaskList() {
       <form method="post" className="space-y-2" onSubmit={handleCreateTask}>
         <Label htmlFor="title">Title Task</Label>
         <Input type="text" name="title" id="title" required />
+
+        <div className="w-36">
+          <Label htmlFor="time-picker" className="px-1">
+            Time
+          </Label>
+          <Input
+            type="time"
+            id="time-picker"
+            step="1"
+            defaultValue="10:30:00"
+            className="bg-background appearance-none [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none"
+          />
+        </div>
 
         <Button>Create Task</Button>
       </form>
